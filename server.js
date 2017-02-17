d8225322c54fce96619b2b2b02ad7ff00b10fdcf
Muthuraman Lakshmanan <Muthuraman Lakshmanan>
2017-02-17 12:23:38 +0530

    enabled cors

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
